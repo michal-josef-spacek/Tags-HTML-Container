2466118683dcfbfb552ff881b9d7b5a970adfe85
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-05-30 09:00:33 +0200

    Prepare for 0.03 version

--- Container.pm
+++ Container.pm
@@ -19,7 +19,7 @@ Readonly::Hash our %VERT_CONV => (
 	'top' => 'flex-start',
 );
 
-our $VERSION = 0.02;
+our $VERSION = 0.03;
 
 sub new {
 	my ($class, @params) = @_;
@@ -286,6 +286,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.02
+0.03
 
 =cut
